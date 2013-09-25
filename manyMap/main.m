05c300608f330a09fff5a5a1581cf341e7646593
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-09-24 19:24:57 -0600

    Hey Rockey Look! An Xcode Project!

---
+++ manyMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  manyMap
+//
+//  Created by Kepler Sticka-Jones on 9/24/13.
+//  Copyright (c) 2013 Kepler Sticka-Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMAppDelegate class]));
+    }
+}
---
+++ manyMapTests/manyMapTests.m
@@ -0,0 +1,34 @@
+//
+//  manyMapTests.m
+//  manyMapTests
+//
+//  Created by Kepler Sticka-Jones on 9/24/13.
+//  Copyright (c) 2013 Kepler Sticka-Jones. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface manyMapTests : XCTestCase
+
+@end
+
+@implementation manyMapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
